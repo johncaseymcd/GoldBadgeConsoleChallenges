0d864dca59541daa28f7baf54d2faa09cddb345d
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-09 21:39:09 -0500

    Eleventh overall commit, fourth commit for Komodo Claim. Added new namespace to hold Program and ProgramUI for Claims challenge. Renamed namespace for Cafe challenge to keep naming conventions consistent.

---
+++ GoldBadgeConsoleChallengeClaimConsole/KomodoClaim_Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBadgeConsoleChallengeClaimConsole
+{
+    class KomodoClaimProgram
+    {
+        static void Main(string[] args)
+        {
+            var program = new KomodoClaimProgramUI();
+            program.Run();
+        }
+    }
+}
