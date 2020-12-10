279713dd7b194a80dca75450fd7b350418664643
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-09 21:20:33 -0500

    Ninth overall commit, third commit for Komodo Claims. Finished building and running test methods for ClaimCRUD class methods. All tests pass.

--- GoldBadgeConsoleAppClasses/KomodoClaims_ClaimClass.cs
+++ GoldBadgeConsoleAppClasses/KomodoClaims_ClaimClass.cs
@@ -6,7 +6,106 @@ using System.Threading.Tasks;
 
 namespace GoldBadgeConsoleAppClasses
 {
-    class Class1
+    public enum ClaimType
     {
+        Car,
+        Home, 
+        Theft
+    }
+
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType ClaimType{ get; set; }
+        public string ClaimDescription { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime LossDate { get; set; }
+        public DateTime ClaimDate { get; set; }
+        public bool IsValid { get; set; }
+
+        public Claim() { }
+        public Claim(int claimID, ClaimType claimType, string claimDescription, decimal claimAmount, DateTime lossDate, DateTime claimDate, bool isValid)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            ClaimDescription = claimDescription;
+            ClaimAmount = claimAmount;
+            LossDate = lossDate;
+            ClaimDate = claimDate;
+            IsValid = isValid;
+        }
+    }
+
+    public class ClaimCRUD
+    {
+        private Queue<Claim> _allClaims = new Queue<Claim>();
+
+        // Helper method to find claims by Claim ID
+        public Claim FindClaimByID(int claimID)
+        {
+            foreach (var claim in _allClaims)
+            {
+                if (claim.ClaimID == claimID)
+                {
+                    return claim;
+                }
+            }
+
+            return null;
+        }
+        // Create a new claim and add it to the queue
+        public void CreateClaim(Claim newClaim)
+        {
+            _allClaims.Enqueue(newClaim);
+        }
+
+        // Return all claims in the queue 
+        public Queue<Claim> GetAllClaims()
+        {
+            return _allClaims;
+        }
+
+        // Edit an existing claim in the queue
+        public bool EditClaim(int claimID, Claim newClaim)
+        {
+            var editClaim = FindClaimByID(claimID);
+            if (editClaim != null)
+            {
+                editClaim.ClaimID = newClaim.ClaimID;
+                editClaim.ClaimType = newClaim.ClaimType;
+                editClaim.ClaimDescription = newClaim.ClaimDescription;
+                editClaim.ClaimAmount = newClaim.ClaimAmount;
+                editClaim.LossDate = newClaim.LossDate;
+                editClaim.ClaimDate = newClaim.ClaimDate;
+                editClaim.IsValid = newClaim.IsValid;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Delete a claim from the queue
+        public bool DeleteClaim(int claimID)
+        {
+            foreach(var claim in _allClaims)
+            {
+                if (claim.ClaimID == claimID)
+                {
+                    var deletedClaim = _allClaims.Dequeue();
+                    if (deletedClaim != null)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
--- GoldBadgeConsoleAppTests/KomodoClaims_ClaimTest.cs
+++ GoldBadgeConsoleAppTests/KomodoClaims_ClaimTest.cs
@@ -1,14 +1,91 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using GoldBadgeConsoleAppClasses;
 
 namespace GoldBadgeConsoleAppTests
 {
     [TestClass]
-    public class UnitTest1
+    public class KomodoClaimsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddClaim()
         {
+            // Arrange (Create and initialize variables)
+            var lossDate = new DateTime(2020, 03, 14);
+            var claimDate = new DateTime(2020, 03, 15);
+            var claimExpiration = new TimeSpan(30, 0, 0, 0);
+            bool isValid = claimDate <= lossDate + claimExpiration;
+            var claim = new Claim(1, ClaimType.Car, "Fender bender", 3000.00m, lossDate, claimDate, isValid);
+            var claimTester = new ClaimCRUD();
+            int beginningCount = claimTester.GetAllClaims().Count;
+
+            // Act (Add new claim to queue)
+            claimTester.CreateClaim(claim);
+
+            // Assert (Check the count of the claims queue to make sure it is greater than it was at the beginning)
+            int endingCount = claimTester.GetAllClaims().Count;
+            Assert.IsTrue(endingCount > beginningCount, "Add was not successful.");
+        }
+
+        [TestMethod]
+        public void TestReadClaim()
+        {
+            // Arrange (Create and initialize variables)
+            var lossDate = new DateTime(2019, 08, 12);
+            var claimDate = new DateTime(2019, 09, 09);
+            var claimExpiration = new TimeSpan(30, 0, 0, 0);
+            bool isValid = claimDate <= lossDate + claimExpiration;
+            var claim = new Claim(1, ClaimType.Home, "House fire", 150000.00m, lossDate, claimDate, isValid);
+            var claimTester = new ClaimCRUD();
+
+            // Act (Add claim to queue)
+            claimTester.CreateClaim(claim);
+
+            // Assert (List of claims should not be null)
+            Assert.IsNotNull(claimTester.GetAllClaims(), "Read was not successful");
+        }
+
+        [TestMethod]
+        public void TestEditClaim()
+        {
+            // Arrange (Create and initialize variables)
+            var oldLossDate = new DateTime(2020, 05, 04);
+            var oldClaimDate = new DateTime(2020, 06, 09);
+            var oldClaimExpiration = new TimeSpan(30, 0, 0, 0);
+            bool oldIsValid = oldClaimDate <= oldLossDate + oldClaimExpiration;
+            var oldClaim = new Claim(1, ClaimType.Home, "Pipe burst", 2000.00m, oldLossDate, oldClaimDate, oldIsValid);
+            var claimTester = new ClaimCRUD();
+            claimTester.CreateClaim(oldClaim);
+
+            // Act (Create a new claim to pass into the edit method)
+            var newLossDate = new DateTime(2020, 01, 21);
+            var newClaimDate = new DateTime(2020, 02, 02);
+            var newClaimExpiration = new TimeSpan(30, 0, 0, 0);
+            bool newIsValid = newClaimDate <= newLossDate + newClaimExpiration;
+            var newClaim = new Claim(2, ClaimType.Car, "Total loss", 35000.00m, newLossDate, newClaimDate, newIsValid);
+            bool wasEdited = claimTester.EditClaim(oldClaim.ClaimID, newClaim);
+
+            // Assert (wasEdited should return a value of true if the edit was successful)
+            Assert.IsTrue(wasEdited, "Edit was not successful.");
+        }
+
+        [TestMethod]
+        public void TestDeleteClaim()
+        {
+            // Arrange (Create and initialize variables)
+            var lossDate = new DateTime(2020, 08, 11);
+            var claimDate = new DateTime(2020, 09, 01);
+            var claimExpiration = new TimeSpan(30, 0, 0, 0);
+            bool isValid = claimDate <= lossDate + claimExpiration;
+            var claim = new Claim(1, ClaimType.Theft, "Stolen TV and laptop", 2500.00m, lossDate, claimDate, isValid);
+            var claimTester = new ClaimCRUD();
+            claimTester.CreateClaim(claim);
+
+            // Act (Call DeleteClaim() method on the claim
+            bool wasDeleted = claimTester.DeleteClaim(claim.ClaimID);
+
+            // Assert (wasDeleted should return true if the delete was successful)
+            Assert.IsTrue(wasDeleted, "Delete was not successful.");
         }
     }
 }
