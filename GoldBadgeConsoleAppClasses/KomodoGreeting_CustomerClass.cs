23f07dac8e27891f07f86f1d1a091cf467e4e625
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-15 18:20:24 -0500

    Commit 25/5.0. Initial commit for Komodo Greeting challenge. Added new classes and files pertaining to project (unit test, class library, console app). Built Customer and CustomerCRUD classes and methods. Created basic skeleton for ProgramUI class. Going to build test methods for CustomerCRUD class before next commit. Now playing: Embrasure by Cruel Diagnosis.

---
+++ GoldBadgeConsoleAppClasses/KomodoGreeting_CustomerClass.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBadgeConsoleAppClasses
+{
+    public enum CustomerType
+    {
+        Current = 1,
+        Past,
+        Potential
+    }
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerType Type { get; set; }
+        public string EmailToSend { get; set; }
+
+        public Customer() { }
+
+        public Customer(string first, string last, CustomerType type)
+        {
+            FirstName = first;
+            LastName = last;
+            Type = type;
+            switch (type)
+            {
+                case CustomerType.Current:
+                    EmailToSend = "Thank you for your work with us. We appreciate your loyalty. Here's a coupon.";
+                    break;
+                case CustomerType.Past:
+                    EmailToSend = "It's been a long time since we've heard from you, we want you back.";
+                    break;
+                case CustomerType.Potential:
+                    EmailToSend = "We currently have the lowest rates on Helicopter Insurance!";
+                    break;
+            }
+        }
+    }
+
+    public class CustomerCRUD
+    {
+        private SortedList<string, Customer> _customerList = new SortedList<string, Customer>();
+
+        // Helper method to find customers by name
+        private Customer FindCustomerByName(string lastName, string firstName)
+        {
+            foreach (var customer in _customerList)
+            {
+                if (customer.Key == lastName && customer.Value.FirstName == firstName)
+                {
+                    return customer.Value;
+                }
+            }
+
+            return null;
+        }
+
+        // Create
+        public void CreateCustomer(Customer newCustomer)
+        {
+            _customerList.Add(newCustomer.LastName, newCustomer);
+        }
+
+        // Read
+        public SortedList<string, Customer> GetAllCustomers()
+        {
+            return _customerList;
+        }
+
+        // Update
+        public bool EditExistingCustomer(Customer oldCustomer)
+        {
+            var editCustomer = FindCustomerByName(oldCustomer.LastName, oldCustomer.FirstName);
+            if (editCustomer != null)
+            {
+                editCustomer.FirstName = oldCustomer.FirstName;
+                editCustomer.LastName = oldCustomer.LastName;
+                editCustomer.Type = oldCustomer.Type;
+                editCustomer.EmailToSend = oldCustomer.EmailToSend;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Delete
+        public bool DeleteCustomer(Customer oldCustomer)
+        {
+            var deleteCustomer = FindCustomerByName(oldCustomer.LastName, oldCustomer.FirstName);
+            if (deleteCustomer != null)
+            {
+                _customerList.Remove(oldCustomer.LastName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GoldBadgeConsoleChallengeConsole_Email/KomodoGreeting_ProgramUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoldBadgeConsoleAppClasses;
+
+namespace GoldBadgeConsoleChallengeConsole_Email
+{
+    class KomodoGreetingProgramUI
+    {
+        private SortedList<string, Customer> _allCustomers = new SortedList<string, Customer>();
+
+        public void Run()
+        {
+
+        }
+
+        private void MainMenu()
+        {
+
+        }
+
+        // Helper method to handle user input errors
+        private void PressAnyKey()
+        {
+            Console.WriteLine("Invalid input. Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        private void SeedCustomerList()
+        {
+
+        }
+    }
+}
