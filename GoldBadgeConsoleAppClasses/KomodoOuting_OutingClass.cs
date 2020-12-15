41c5344a566c999dd2978dfcefc49c351f3f6b35
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-14 22:14:29 -0500

    Commit 21/4.1. Tested OutingCRUD methods, all tests pass. Going to work on ProgramUI methods before next commit.

--- GoldBadgeConsoleAppClasses/KomodoOuting_OutingClass.cs
+++ GoldBadgeConsoleAppClasses/KomodoOuting_OutingClass.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace GoldBadgeConsoleAppClasses
 {
-    class KomodoOuting_OutingClass
+    public enum OutingType
     {
+        Amusement_Park = 1,
+        Bowling,
+        Concert,
+        Golf
+    }
+        
+    public class Outing
+    {
+        public DateTime Date { get; set; }
+        public OutingType Type { get; set; }
+        public int Attendance { get; set; }
+        public decimal CostPerPerson { get; set; }
+        public decimal TotalCost { get; set; }
+
+        public Outing() { }
+
+        public Outing(DateTime date, OutingType type, int attendance)
+        {
+            Date = date;
+            Type = type;
+            Attendance = attendance;
+            switch (type)
+            {
+                case OutingType.Amusement_Park:
+                    CostPerPerson = 60.00m;
+                    break;
+                case OutingType.Bowling:
+                    CostPerPerson = 25.00m;
+                    break;
+                case OutingType.Concert:
+                    CostPerPerson = 75.00m;
+                    break;
+                case OutingType.Golf:
+                    CostPerPerson = 150.00m;
+                    break;
+            }
+            TotalCost = CostPerPerson * attendance;
+        }
+    }
+
+    public class OutingCRUD
+    {
+        SortedList<DateTime, Outing> _listOfOutings = new SortedList<DateTime, Outing>();
+
+        // Create
+        public void CreateNewOuting(Outing newOuting)
+        {
+            _listOfOutings.Add(newOuting.Date, newOuting);
+        }
+
+        // Read
+        public SortedList<DateTime, Outing> GetAllOutings()
+        {
+            return _listOfOutings;
+        }
+
+        // Per scope of challenge, no update or delete methods are necessary
     }
 }
--- GoldBadgeConsoleAppTests/KomodoOuting_OutingTest.cs
+++ GoldBadgeConsoleAppTests/KomodoOuting_OutingTest.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
+using GoldBadgeConsoleAppClasses;
+using System.Collections.Generic;
 
 namespace GoldBadgeConsoleAppTests
 {
     [TestClass]
-    public class UnitTest2
+    public class KomodoOuting_OutingTest
     {
+        OutingCRUD outingTester = new OutingCRUD();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddNewOutingAndGetAllOutings()
         {
+            // Arrange (initialize variables)
+            var newOutingDate = new DateTime(2020, 04, 07, 12, 00, 00);
+            var newOuting = new Outing(newOutingDate, OutingType.Golf, 30);
+            int beginningCount = 0;
+
+            // Act (add new Outing to the list)
+            outingTester.CreateNewOuting(newOuting);
+
+            // Assert (that the list of outings is not null, and that the count is greater than 0
+            int endingCount = outingTester.GetAllOutings().Count; // Also test GetAllOutings method to make sure it is capturing the list properly
+
+            Assert.IsNotNull(outingTester.GetAllOutings(), "Add failed.");
+            Assert.IsTrue(endingCount > beginningCount, "Add failed.");
         }
     }
 }
