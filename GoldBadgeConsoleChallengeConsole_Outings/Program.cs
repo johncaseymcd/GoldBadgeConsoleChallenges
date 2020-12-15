0ae31407d2a3665eb72feaa2683cf25b6fe53511
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-14 21:45:03 -0500

    Commit 20/4.0. Built Outing and OutingCRUD classes, built skeleton for ProgramUI class. Created UnitTest file, going to run tests before next commit.

---
+++ GoldBadgeConsoleChallengeConsole_Outings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoldBadgeConsoleAppClasses;
+
+namespace GoldBadgeConsoleChallengeConsole_Outings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new ProgramUI();
+            program.Run();
+        }
+    }
+}
---
+++ GoldBadgeConsoleChallengeConsole_Outings/ProgramUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBadgeConsoleChallengeConsole_Outings
+{
+    class ProgramUI
+    {
+        public void Run()
+        {
+            SeedOutingsList();
+            MainMenu();
+        }
+
+        private void MainMenu()
+        {
+
+        }
+
+
+
+        private void SeedOutingsList()
+        {
+
+        }
+    }
+}
